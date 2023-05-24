74ee8067ba18f48bbabc64f4d44924450f3261a1
billp <billpiazzetta@gmail.com>
2023-05-24 15:28:08 -0300

    Lista renderizando, parcialmente estilizada

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -1,38 +1,67 @@
-import React, { useState } from 'react';
-import TaskCard from './TaskCard';
+import React, { useState } from "react";
+import TaskCard from "./TaskCard";
 
 export default function ListaTarefas() {
-    const [listaTarefas, setListaTarefas] = useState([]);
-    const [tarefa, setTarefa] = useState('');
+  const [listaTarefas, setListaTarefas] = useState([]);
+  const [tarefa, setTarefa] = useState("");
 
-    const inserirTarefaHandler = (tarefa) => {
-        setListaTarefas([...listaTarefas, tarefa]);
-        console.log(listaTarefas);
-    }
+  const inserirTarefaHandler = (tarefa) => {
+    setListaTarefas([...listaTarefas, tarefa]);
+    console.log(listaTarefas);
+  };
 
-    const handleChange = event => {
-        setTarefa(event.target.value);
-        console.log(event.target.value);
-    }
+  const handleChange = (event) => {
+    setTarefa(event.target.value);
+    console.log(event.target.value);
+  };
 
-    // const removerTarefaHandler = (tarefa) => {
+  const removerTarefaHandler = (tarefa) => {
+    console.log(`Removi item ${tarefa}`);
+  };
 
-    // }
-
-    return (
-        <div>
-            <h1>Lista de Tarefas</h1>
-            <input 
-                type="text" 
-                onChange={handleChange}
-                value={tarefa}
-            />
-            <button 
-                onClick={() => inserirTarefaHandler(tarefa)}
-            >Inserir Tarefa</button>
-            <ul>
-
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <input type="text" onChange={handleChange} value={tarefa} style={styles.input}/>
+      <button onClick={() => inserirTarefaHandler(tarefa)} style={styles.button}>
+        Inserir Tarefa
+      </button>
+      <ul>
+        {listaTarefas.map((tarefa) => {
+          return (
+            <li style={styles.list}>
+              <TaskCard
+                textoTarefa={tarefa}
+                deleteFunction={removerTarefaHandler}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
+
+const styles = {
+    input: {
+        margin: 10,
+        padding: 8,
+        boxShadow: "1px 1px 2px #778da9",
+        backgroundColor: "white"
+    },
+    button: {
+        backgroundColor: '#4CAF50', /* Green */
+        border: 'none',
+        color: 'white',
+//   padding: 15px 32px;
+//   text-align: center;
+//   text-decoration: none;
+//   display: inline-block;
+//   font-size: 16px;
+    },
+    list: {
+        listStyleType: 'none'
+    }
+  };
+
+
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function TaskCard(props) {
     return (
-        <div>
-            
+        <div style={styles.card}>
+            <input type="checkbox" name="checkbox"></input>
+            <p>{props.textoTarefa}</p>
+            <button onClick={props.deleteFunction}>X</button>
         </div>
     )
 }
+
+const styles = {
+    card: {
+      margin: 10,
+      padding: 1,
+      boxShadow: "1px 1px 2px #778da9",
+      backgroundColor: "lightblue",
+      display: "flex",
+      flexDirection: "row",
+      width: 'auto',
+      justifyContent: "space-between",
+    }
+  };
