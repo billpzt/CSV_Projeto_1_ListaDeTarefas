66fa559475a62431114cf72bacd31fb40db62a2f
billp <billpiazzetta@gmail.com>
2023-05-28 20:14:51 -0300

    Estilização botões, alternancia entre marcar/desmarcar todas

--- src/components/ListaTarefas.jsx
+++ src/components/ListaTarefas.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TaskCard from "./TaskCard";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 export default function ListaTarefas() {
-
   const [listaTarefas, setListaTarefas] = useState([]);
-  const [tarefaTexto, setTarefaTexto] = useState('');
-  const [tarefa, setTarefa] = useState({id: '', texto: ''});
+  const [tarefaTexto, setTarefaTexto] = useState("");
+  const [tarefa, setTarefa] = useState({ id: "", texto: "" });
+  const [marcadas, setMarcadas] = useState(false);
+  const [textoMarcadas, setTextoMarcadas] = useState('Marcar todas');
 
   const inserirTarefaHandler = (tarefa) => {
-    const novaTarefa = {...tarefa};
+    const novaTarefa = { ...tarefa };
     setListaTarefas([...listaTarefas, novaTarefa]);
     console.log(listaTarefas);
   };
@@ -20,35 +20,78 @@ export default function ListaTarefas() {
   };
 
   const handleRemoverTarefa = (value) => {
-    const novaListaTarefas = listaTarefas.filter(tarefa => tarefa.id !== value);
-    setListaTarefas(novaListaTarefas);
-  };
-
-  const handleRemoverMarcadas = () => {
-    var novaListaTarefas = listaTarefas.filter(
-      tarefa => !tarefa.checked
+    const novaListaTarefas = listaTarefas.filter(
+      (tarefa) => tarefa.id !== value
     );
     setListaTarefas(novaListaTarefas);
   };
 
+  const handleRemoverMarcadas = () => {
+    var novaListaTarefas = listaTarefas.filter((tarefa) => !tarefa.checked);
+    setListaTarefas(novaListaTarefas);
+  };
+
+  const handleCheckboxes = () => {
+    if (!marcadas) {
+      marcarTodas();
+      setMarcadas(true);
+      setTextoMarcadas("Desmarcar todas");
+    } else {
+      desmarcarTodas();
+      setMarcadas(false);
+      setTextoMarcadas("Marcar todas");
+    }
+  }
+
   const marcarTodas = () => {
-    var checkboxes = document.getElementsByName('checkbox');
+    var checkboxes = document.getElementsByName("checkbox");
     var checkboxesArray = Array.from(checkboxes);
     checkboxesArray.map((item) => {
-      return item.checked = true;
-    })
-  }
+      return (item.checked = true);
+    });
+  };
+
+  const desmarcarTodas = () => {
+    var checkboxes = document.getElementsByName("checkbox");
+    var checkboxesArray = Array.from(checkboxes);
+    checkboxesArray.map((item) => {
+      return (item.checked = false);
+    });
+  };
+
+  useEffect(() => {}, [listaTarefas]);
 
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-      <input type="text" onChange={handleChange} value={tarefaTexto} style={styles.input}/>
-      <button onClick={() => inserirTarefaHandler({id: uuidv4(), texto: tarefaTexto})} style={styles.button}>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={tarefaTexto}
+        style={styles.input}
+      />
+      
+      <div>
+      <button
+        onClick={() =>
+          inserirTarefaHandler({ id: uuidv4(), texto: tarefaTexto })
+        }
+        style={styles.button}
+      >
         Inserir Tarefa
       </button>
-      <div>
-        <button onClick={() => handleRemoverMarcadas()}>Excluir tarefas marcadas</button>
-        <button onClick={() => {marcarTodas()}}>Marcar todas</button>
+      <button
+          onClick={() => handleCheckboxes()}
+          style={styles.buttonMarcarTodas}
+        >
+          {textoMarcadas}
+        </button>
+        <button
+          onClick={() => handleRemoverMarcadas()}
+          style={styles.buttonExcluirTarefas}
+        >
+          Excluir tarefas marcadas
+        </button>
       </div>
       <ul>
         {listaTarefas.map((tarefa) => {
@@ -68,26 +111,44 @@ export default function ListaTarefas() {
 }
 
 const styles = {
-    input: {
-        margin: 10,
-        padding: 15,
-        width: '80%',
-        boxShadow: "1px 1px 2px #778da9",
-        backgroundColor: "white"
-    },
-    button: {
-        backgroundColor: '#4CAF50', /* Green */
-        border: 'none',
-        color: 'white',
-        padding: 15,
-        textAlign: 'center',
-        textDecoration: 'none',
-        display: 'inline-block',
-        fontSize: 16
-    },
-    list: {
-        listStyleType: 'none'
-    }
-  };
-
-
+  input: {
+    margin: 10,
+    padding: 15,
+    width: "80%",
+    boxShadow: "1px 1px 2px #778da9",
+    backgroundColor: "white",
+  },
+  button: {
+    backgroundColor: "#4CAF50" /* Green */,
+    border: "none",
+    color: "white",
+    padding: 15,
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: 16,
+  },
+  buttonExcluirTarefas: {
+    backgroundColor: "red",
+    border: "none",
+    color: "white",
+    padding: 15,
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: 16,
+  },
+  buttonMarcarTodas: {
+    backgroundColor: "blue",
+    border: "none",
+    color: "white",
+    padding: 15,
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: 16,
+  },
+  list: {
+    listStyleType: "none",
+  },
+};
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,9 +5,14 @@ export default function TaskCard(props) {
 
     return (
         <div style={styles.card}>
-            <input type="checkbox" name="checkbox"></input>
+            <input 
+                type="checkbox" 
+                name="checkbox"
+                style={styles.checkBox}></input>
             <p>{props.textoTarefa}</p>
-            <button onClick={props.deleteFunction}>X</button>
+            <button 
+                onClick={props.deleteFunction}
+                style={styles.xButton}>X</button>
         </div>
     )
 }
@@ -16,11 +21,25 @@ const styles = {
     card: {
       margin: 10,
       padding: 1,
+      paddingRight: 15,
+      paddingLeft: 15,
       boxShadow: "1px 1px 2px #778da9",
       backgroundColor: "lightblue",
       display: "flex",
       flexDirection: "row",
       width: 'auto',
       justifyContent: "space-between",
+      fontSize: 20
+    //   alignItems: "center"
+    },
+    checkBox: {
+        width:25
+    },
+    xButton: {
+        width: 45,
+        height: 45,
+        fontSize: 30,
+        marginTop: 8,
+        marginBottom: 8
     }
   };
